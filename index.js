b989264a821a21d584c2c74f0c82a0bcea61d0e7
RoyOkite <royokite@gmail.com>
2022-08-17 16:38:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+// Scope lab!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavouriteCustomer = 'wanda';
+
+function changeLeastFavoriteCustomer() {
+    leastFavouriteCustomer = 'scrolls';
+}
